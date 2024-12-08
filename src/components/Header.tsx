b2519668f90684aa1ecbcd2033394e7964aaf805
StokinDenis <karina-romanova-05@bk.ru>
2024-12-08 16:54:59 +0300

    add routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../styles/Header.scss';
+import { Link } from "react-router-dom";
 
 const Header: React.FC=()=>{
 
@@ -8,9 +9,9 @@ const Header: React.FC=()=>{
         <div className="header__logo">5S Control</div>
         <nav className="header__menu menu">
             <ul>
-                <li><a href="#home">Главная</a></li>
-                <li><a href="#tasks">Задачи 5S</a></li>
-                <li><a href="#contacts">Контакты</a></li>
+                <li><Link to="/">Главная</Link></li>
+                <li><Link to="/tasks">Задачи 5S</Link></li>
+                <li><Link to="/contacts">Контакты</Link></li>
             </ul>
         </nav>
     </header>
