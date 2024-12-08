1649470616ea1b2fa4f059b97feb012e3bf54945
StokinDenis <karina-romanova-05@bk.ru>
2024-12-08 15:56:56 +0300

    creating component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header: React.FC=()=>{
+
+    return(
+    <header className="header">
+        <div className="header__logo">5S Control</div>
+        <nav className="header__menu menu">
+            <ul>
+                <li><a href="#home">Главная</a></li>
+                <li><a href="#tasks">Задачи 5S</a></li>
+                <li><a href="#contacts">Контакты</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+}
+
+export default Header
