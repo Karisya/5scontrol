20c3cb61b56588808a475d5b994a364d7399e16e
StokinDenis <karina-romanova-05@bk.ru>
2024-12-10 01:47:06 +0300

    creating page for tasks

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Tasks from "../components/Tasks";
+import '../styles/TaskPage.scss';
 
 const TasksPage:React.FC=()=>{
 
 return(
-    <div></div>
+    <div className="task-page">
+        <h2 className="task-page__header">Задачи 5S</h2>
+        <Tasks/>
+    </div>
 )
 }
 
