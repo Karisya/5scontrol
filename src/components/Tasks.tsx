795c3b28b430aea47ee39af92f85059656826094
StokinDenis <karina-romanova-05@bk.ru>
2024-12-10 10:56:04 +0300

    adding the filter for tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -18,6 +18,12 @@ const Tasks:React.FC=()=>{
     const showForm = useSelector((state: { showForm: { showForm: boolean } }) => state.showForm.showForm);
     const showModal = useSelector((state: { showModal: { showModal: boolean } }) => state.showModal.showModal);
 
+    const [filter, setFilter] = useState<string>("Все");
+    const filteredTasks = tasks.filter((task) => {
+      if (filter === "Все") return true; 
+      return task.status === filter; 
+    });
+
     const dispatch = useDispatch();
 
     const hanbleShowForm=()=>{
@@ -39,35 +45,58 @@ const Tasks:React.FC=()=>{
             <div className="tasks__btn-holder">
                 <button className="tasks__add-btn" onClick={hanbleShowForm}>Добавить</button>
             </div>
+            <select className="tasks__filter"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}>
+          <option value="Все">Все</option>
+          <option value="Новая">Новая</option>
+          <option value="В работе">В работе</option>
+          <option value="Завершена">Завершена</option>
+        </select>
             {showModal&&<div className="modal-overlay"><Modal/></div>}
             {showForm && (
             <div className="modal-overlay"><TaskForm /></div>)
             }
-            {(tasks.length===0)?<div className="tasks__none">Пусто</div>:(<table>
-        <thead>
-          <tr>
-            <th>ID задачи</th>
-            <th>Название задачи</th>
-            <th>Статус</th>
-            <th>Дата создания</th>
-            <th>Действия</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.id}</td>
-              <td>{task.name}</td>
-              <td>{task.status}</td>
-              <td>{task.date}</td>
-              <td>
-                <div className="edit-btn" onClick={() => handleEditTask(task)}><img src={editIcon} alt="Редактировать" /></div>
-                <div className="delete-btn" onClick={() => handleDeleteTask(task.id)}><img src={deleteIcon} alt="Удалить" /></div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>)}
+            {<div>
+              {tasks.length === 0 ? (
+                <div className="tasks__none">Пусто</div>) : (
+                  <>
+                     {filteredTasks.length === 0 ? (
+                         <div className="tasks__none">Нет задач с выбранным статусом</div> ) : (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>ID задачи</th>
+                        <th>Название задачи</th>
+                        <th>Статус</th>
+                        <th>Дата создания</th>
+                        <th>Действия</th>
+                      </tr>
+                    </thead>
+                   <tbody>
+                      {filteredTasks.map((task) => (
+                        <tr key={task.id}>
+                          <td>{task.id}</td>
+                          <td>{task.name}</td>
+                          <td>{task.status}</td>
+                          <td>{task.date}</td>
+                          <td>
+                            <div className="edit-btn" onClick={() => handleEditTask(task)}>
+                              <img src={editIcon} alt="Редактировать" />
+                            </div>
+                            <div className="delete-btn" onClick={() => handleDeleteTask(task.id)}>
+                              <img src={deleteIcon} alt="Удалить" />
+                            </div>
+                          </td>
+                        </tr>
+                              ))}
+                     </tbody>
+                    </table>
+      )}
+    </>
+  )}
+</div>
+}
         </div>
     )
 }
