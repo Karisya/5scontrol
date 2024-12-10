12537507bde8043ed76ddafa974bf4852c47985d
StokinDenis <karina-romanova-05@bk.ru>
2024-12-10 11:54:09 +0300

    creating menu-burder

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/Header.scss';
 import { Link } from "react-router-dom";
 
 const Header: React.FC=()=>{
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
     return(
     <header className="header">
         <div className="header__logo">5S Control</div>
-        <nav className="header__menu menu">
+        <nav className={`header__menu menu ${isMenuOpen ? 'open' : ''}`}>
             <ul>
                 <li><Link to="/">Главная</Link></li>
                 <li><Link to="/tasks">Задачи 5S</Link></li>
                 <li><Link to="/contacts">Контакты</Link></li>
             </ul>
         </nav>
+        <div className="header__burger" onClick={toggleMenu}>
+        <span className="burger-line"></span>
+        <span className="burger-line"></span>
+        <span className="burger-line"></span>
+      </div>
     </header>
 )
 
