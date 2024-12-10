62660a9ef9e974e3157a17508e104789c2e18a10
StokinDenis <karina-romanova-05@bk.ru>
2024-12-10 09:47:46 +0300

    update pages

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import ContactForm from "../components/ContactForm";
+import '../styles/ContactPage.scss';
 
 const ContactPage:React.FC=()=>{
 
 return(
-    <div></div>
+    <div className="contact-page">
+        <h2>Свяжитесь с нами:</h2>
+        <ContactForm/>
+    </div>
     )
 }
 
